27dc03f1bf910f58e629e9538587ecea91c0e0c0
Emiliano Ciavatta <emiliano.ciavatta@studio.unibo.it>
2020-10-25 22:12:37 +0100

    Add mobile style

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,22 @@ window.onload = function () {
         strings: ["eciavatta$"],
         typeSpeed: 40
     };
-    
+
     new Typed('.typed', options);
+
+    var menuButton = document.getElementById("menu-button");
+    var menu = document.getElementById("main-menu"); 
+
+    console.log(menuButton)
+
+    menuButton.onclick = function () {
+        if (menu.classList.contains("is-active")) {
+            menu.classList.remove("is-active");
+            menuButton.classList.remove("is-active")
+        } else {
+            menu.classList.add("is-active");
+            menuButton.classList.add("is-active")
+        }
+    }
+    
 }
